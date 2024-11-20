9ac211f462c48b0857118fc8f30a454c0209b221
chame <angelchame6@gmail.com>
2024-11-20 07:40:57 -0600

    organizacion de codigo

---
+++ src/app/Chofer/card-horarios/card-horarios.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit,Input} from '@angular/core';
+import { Router } from '@angular/router';
+import { RegistroRutaService } from '../../service/registro-ruta.service';
+import { Horarios } from '../../interface/horarios';
+@Component({
+  selector: 'app-card-horarios',
+  templateUrl: './card-horarios.component.html',
+  styleUrl: './card-horarios.component.css'
+})
+export class CardHorarioComponent implements OnInit {
+  @Input() unidades: string[] = [];
+  horarios: Horarios[] = [];
+  horariosOriginales: Horarios[] = [];
+  terminalSeleccionada: string = '';
+
+  constructor(private horarioService: RegistroRutaService, private router: Router) {}
+
+  ngOnInit() {
+    this.horarioService.informacion$.subscribe((data) => {
+      this.horariosOriginales = data;
+      this.horarios = [...this.horariosOriginales];
+      this.filtrarHorarios();
+    });
+
+    this.horarioService.terminalSeleccionada$.subscribe((terminal) => {
+      this.terminalSeleccionada = terminal;
+      this.filtrarHorarios();
+    });
+  }
+
+  filtrarHorarios() {
+    if (this.terminalSeleccionada) {
+      this.horarios = this.horariosOriginales.filter(
+        (horario) => horario.viaje === this.terminalSeleccionada
+      );
+    } else {
+      this.horarios = [...this.horariosOriginales];
+    }
+  }
+
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AutentificacionModule } from './autentificacion/autentificacion.module';
+import { UsuarioModule } from './Usuarios/usuario/usuario.module';
+import { ChoferesModule } from './Chofer/choferes/choferes.module';
+
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+ 
+   
+  
+  
+   
+    
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AutentificacionModule,
+    UsuarioModule,
+    ChoferesModule
+  ],
+  exports:[
+   
   ],
   providers: [
     provideClientHydration()
